65e214177af95bb54b6bff2a7cc290b1f77d4eeb
Oleg152 <aforusinski@gmai.com>
2022-01-27 18:08:46 +0100

    1.4.3.1 Added constants to DefaultCountingOutRhymer.java and IntLinkedList.java

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,39 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	private static final int FULL = 11;
 
-	public int total = -1;
+	private static final int DEFAULT = -1;
+
+	private static final int NUMBERS_SIZE = 12;
+
+	private int[] numbers = new int[NUMBERS_SIZE];
+
+	public int total = DEFAULT;
 
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
-		public boolean callCheck() {
-			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return numbers[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return numbers[total--];
-					}
+	public boolean callCheck() {
+		return total == DEFAULT;
+	}
+	
+	public boolean isFull() {
+		return total == FULL;
+	}
+
+	protected int peekaboo() {
+		if (callCheck())
+			return DEFAULT;
+		return numbers[total];
+	}
+
+	public int countOut() {
+		if (callCheck())
+			return DEFAULT;
+		return numbers[total--];
+	}
 
 }
